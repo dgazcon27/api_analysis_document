e759120ce6177615a32616681539e2dbc722beba
Daniel Gazcon <daniel.gazcon27@gmail.com>
2021-03-09 18:56:50 -0400

    ngram method, extend token expired date

--- app/documents/api_v1_0/resources.py
+++ app/documents/api_v1_0/resources.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource, reqparse
 from flask_jwt_extended import jwt_required, get_jwt
 from werkzeug import datastructures, utils
 import os
+import uuid
 
 from .schemas import DocumentsSchema
 from ..models import Documents
@@ -25,27 +26,42 @@ class DocumentsListResource(Resource):
     @jwt_required()
     def post(self):
         current_user = get_jwt()
+        random_uuid = uuid.uuid4()
         parser = reqparse.RequestParser()
         parser.add_argument('document', help="File required",
                             required=True, location='files', type=datastructures.FileStorage)
         args = parser.parse_args()
         document = args['document']
+
         file_name = utils.secure_filename(document.filename)
+        name_file = str(random_uuid)+"."+file_name.split(".")[1]
         os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-        file_path = os.path.join(UPLOAD_FOLDER, file_name)
+        file_path = os.path.join(UPLOAD_FOLDER, name_file)
         doc = Documents(
-            url=file_path,
+            url=name_file,
             name=file_name,
             user_id=current_user["user_id"]
         )
-        if doc.get_by_title() is None:
+        if doc.get_by_name() is None:
             doc.save()
             document.save(file_path)
-            response = documents_schema.dump(doc)
-            return response, 201
+            doc.get_stats_coincidence()
+            # response = documents_schema.dump(doc)
+            return {"msg": "ok"}, 201
         else:
             return {"message": {"document": f"Documents {file_name} already exist"}}, 400
 
 
+class TestDocuments(Resource):
+    def get(self):
+        doc = Documents(
+            url="1b5078c4-8451-43e1-a244-28a88811049b.pdf",
+            name="test.pdf",
+            id=1)
+        doc.get_stats_coincidence()
+
+
 api.add_resource(DocumentsListResource,
                  '/api/v1.0/documents/', endpoint='documents_list_resource')
+api.add_resource(TestDocuments,
+                 '/api/v1.0/test/', endpoint='test')
--- app/documents/models.py
+++ app/documents/models.py
@@ -1,5 +1,17 @@
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+from os.path import join
+from io import StringIO
+import re
+from nltk import ngrams
+
 from app.db import db, BaseModelMixin
 from app.common.timestamp_mixin import TimestampMixin
+from config.default import UPLOAD_FOLDER
 
 
 class Documents(db.Model, BaseModelMixin, TimestampMixin):
@@ -8,5 +20,58 @@ class Documents(db.Model, BaseModelMixin, TimestampMixin):
     name = db.Column(db.String(255))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def get_by_title(self):
+    def get_by_name(self):
         return Documents.query.filter_by(name=self.name).first()
+
+    def get_stats_coincidence(self):
+        document_url = join(UPLOAD_FOLDER, self.url)
+        text_process = self.get_text_from_pdf(document_url)
+        documents_list = Documents.query.filter(
+            Documents.name != self.name).all()
+        result_ngram = self.get_ngram_method(text_process, documents_list)
+        print(result_ngram)
+
+    def get_ngram_method(self, doc_up, list_store_doc, n=3):
+        list_ngrams_upload = list(ngrams(doc_up, n))
+        list_result = []
+        list_merge = []
+        for doc in list_store_doc:
+            docs_pro = self.get_text_from_pdf(join(UPLOAD_FOLDER, doc.url))
+            doc_tgram = list(ngrams(docs_pro, n))
+            common = []
+            for gram in list_ngrams_upload:
+                if gram in doc_tgram:
+                    common.append(gram)
+            if len(common) > 0:
+                uniq_gram = self.remove_duplicate(common)
+                total_grams = self.remove_duplicate(
+                    list_ngrams_upload+doc_tgram)
+                total_result = '{:.4f}'.format(
+                    (len(uniq_gram)/len(total_grams))*100)
+                list_result.append(
+                    {"ngram": total_result, "suspect_id": doc.id, "name": doc.name})
+        return list_result
+
+    def get_text_from_pdf(self, url):
+        regex = r"/\t|\n|\/|\,|\.|\:|\;|\(|\)|\{|\}|\?|\¿|\"|\'|\_|\-|\]|\[/g"
+        process_text = None
+        output_string = StringIO()
+        with open(url, 'rb') as in_file:
+            parser = PDFParser(in_file)
+            doc = PDFDocument(parser)
+            rsrcmgr = PDFResourceManager()
+            device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            for page in PDFPage.create_pages(doc):
+                interpreter.process_page(page)
+        process_text = output_string.getvalue()
+        if len(process_text) > 0:
+            process_text = process_text.lower()
+            process_text = re.sub(regex, "", process_text)
+            process_text = re.sub(r'\d+', '', process_text)
+            process_text = re.split("  *", process_text)
+        return process_text
+
+    def remove_duplicate(self, list_of_item):
+        unique = list(dict.fromkeys(list_of_item))
+        return unique
